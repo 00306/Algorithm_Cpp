e09bfe2868e6c4b6d96a7dc0b175b8023f92a6d0
Eric <real.dassy@gmail.com>
2024-04-27 00:11:33 +0900

    [1000]
    입력 방식이 왜이렇게 많은걸까요.

---
+++ Algorithm/BaekJoon/Bronze/[1000]/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by 송지혁 on 4/27/24.
+//
+
+#include<iostream>
+
+int main(int argc, const char * argv[]) {
+    int num1, num2;
+    
+    std::cin >> num1;
+    std::cin >> num2;
+    
+    std::cout << num1+num2 << std::endl;
+    
+    return 0;
+}
