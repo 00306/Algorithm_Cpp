3415f1370d2e2f8a8803d8c35d575365cf58600c
Eric <real.dassy@gmail.com>
2024-04-26 23:47:05 +0900

    Initial Commit

---
+++ Algorithm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by 송지혁 on 4/26/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
