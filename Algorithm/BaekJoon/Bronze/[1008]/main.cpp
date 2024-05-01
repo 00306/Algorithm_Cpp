4795fc4a502d17991d4542213ee2be73cdbc113a
Eric <real.dassy@gmail.com>
2024-05-01 14:08:54 +0900

    [1008] 부동소수점 오차
    float 타입의 정밀도는 7자리까지고 double 타입의 정밀도는 15자리까지이기 때문에 문제를 풀기 위해 double 타입을 선택했어요.

---
+++ Algorithm/BaekJoon/Bronze/[1008]/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by 송지혁 on 5/1/24.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    double a;
+    double b;
+    
+    cin >> a;
+    cin >> b;
+    cout.precision(9);
+    cout << a/b << fixed << endl;
+}
