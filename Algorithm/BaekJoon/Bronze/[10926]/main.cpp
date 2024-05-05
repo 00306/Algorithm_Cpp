d9d219e3e34941ff9b94584580b509458ed02a7f
Eric <real.dassy@gmail.com>
2024-05-05 17:44:05 +0900

    [10926]

---
+++ Algorithm/BaekJoon/Bronze/[10926]/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by 송지혁 on 5/5/24.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char * argv[]) {
+    char input[51];
+    cin >> input;
+    cout << input << "??!" << endl;
+}
