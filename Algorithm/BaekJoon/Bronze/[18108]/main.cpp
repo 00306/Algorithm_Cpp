50511bc5b6d751bed7f8644295ae155eff388a2c
Eric <real.dassy@gmail.com>
2024-05-07 14:23:41 +0900

    [18108]

---
+++ Algorithm/BaekJoon/Bronze/[18108]/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by 송지혁 on 5/7/24.
+//
+
+#include<iostream>
+using namespace std;
+
+
+int main(int argc, char * argv[]) {
+    int year;
+    cin >> year;
+    cout << year - 543 << endl;
+    
+}
