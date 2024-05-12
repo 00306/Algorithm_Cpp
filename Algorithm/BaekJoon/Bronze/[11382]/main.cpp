d0d9344a44f34b4be43b5359201a12bac981e916
Eric <real.dassy@gmail.com>
2024-05-12 17:36:01 +0900

    [11382]
    long double이 가장 실수 범위를 넓게 다루는줄 알고 있었는데,
    long long이라는 신기한 타입을 알게되었다..

---
+++ Algorithm/BaekJoon/Bronze/[11382]/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by 송지혁 on 5/12/24.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char * argv[]) {
+    long long a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c << endl;
+}
