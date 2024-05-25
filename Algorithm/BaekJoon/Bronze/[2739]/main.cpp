58c1ff917498c9586d94481601e760b04fd34e8c
Eric <real.dassy@gmail.com>
2024-05-25 23:05:11 +0900

    [2739]

---
+++ Algorithm/BaekJoon/Bronze/[2739]/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by 송지혁 on 5/25/24.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char * argv[]) {
+    int n;
+    cin >> n;
+    
+    for (int i = 1; i < 10; i++) {
+        cout << n << " * " << i << " = " << n*i << endl;
+    }
+}
