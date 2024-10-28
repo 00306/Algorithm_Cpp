b9712cf42ebfb80d05dbceb40fc68403bac6001a
Eric <real.dassy@gmail.com>
2024-10-28 17:08:46 +0900

    하노이 변형

--- Algorithm/BaekJoon/Gold/[11729]/main.cpp
+++ Algorithm/BaekJoon/Gold/[11729]/main.cpp
@@ -27,12 +27,33 @@ void hanoi(int n, int start, int aux, int finish) {
     hanoi(n-1, aux, start, finish);
 }
 
+// 기둥이 4개 일 때
+
+void hanoi4(int n, int start, int aux1, int aux2, int finish) {
+    if (n == 0) return;
+    
+    if (n == 1) {
+        printf("%d -> %d\n", start, finish);
+        return;
+    }
+    
+    int k = n / 2;
+    
+    hanoi4(k, start, finish, aux2, aux1);
+    hanoi(n-k, start, aux2, finish);
+    hanoi4(k, aux1, start, aux2, finish);
+}
+
+
 int main(int argc, char * argv[]) {
     int n = 0;
     scanf("%d", &n);
     
     int count = countMoves(n);
     printf("%d\n", count);
-    hanoi(n, 1, 2, 3);
+//    hanoi(n, 1, 2, 3);
     
+    hanoi4(n, 1, 2, 3, 4);
 }
+
+
